07cd711ea3ba7535cd3c3a70288b978d8657fce9
Dayvid Castro Lima <116978709+DayvidCastro@users.noreply.github.com>
2023-10-29 19:19:22 -0300

    novo arquivo para cadastramento de adms

---
+++ cadastro_adm.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Requerimento online</title>
+    <link rel="stylesheet" href="requerimento.css">
+</head>
+
+<center>
+<body class="fundo">
+    <div class="form2">
+        <h1 class="titulo">Casdastrar ADM</h1>
+        <form class="input" action="" method="post">
+            <input class="input" type="text" name="matricula" placeholder="Matricula">
+            <input class="input" type="text" name="email" placeholder="Email">
+            <input class="input" type="password" name="senha" placeholder="Senha">
+            <input class="input" type="text" name="nome" placeholder="Nome">
+            <button class="login" name="cadastrar">Cadastrar</button>
+            <button class="login" name="sair">voltar</button>
+        </form>
+    </div>
+
+    <?php
+
+    if (isset($_POST['sair'])) {
+
+        header('Location: pagina_requerimento_adm.php');
+        
+    }
+
+    if (isset($_POST['cadastrar'])) {
+
+        $matricula = $_POST['matricula'];
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+
+        include_once("configuracao.php");
+
+        $query = "INSERT INTO adms (matricula, nome, email, senha) VALUES ('$matricula', '$nome', '$email', '$senha')";
+        $resultado = mysqli_query($conexao, $query);
+
+        if ($resultado) {
+
+            echo "Cadastro realizado com sucesso!";
+            header('Location: pagina_login_adm.php');
+
+        } 
+        
+        else {
+
+            echo "Erro ao cadastrar: " . mysqli_error($conexao);
+
+        }
+    }
+
+    ?>
+</body>
+</center>
+</html>
---
+++ pagina_requerimento_adm.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Requerimento online ADM</title>
+    <link rel="stylesheet" href="requerimento.css">
+</head>
+
+<center>
+
+<body class="fundo">
+
+
+    <div class="sub_cabecalho">
+
+    <h1 class="sub_titulo">Requerimento Online (ADM)</h1>
+
+    </div>
+
+
+    <div class="div_opcao">
+        <form class="" action="" method="post">
+            
+            <button class="opcao" name="ir_para_suap">Ir Para SUAP</button>
+
+            <button class="opcao" name="enviar_requerimento">Ver requerimento</button>
+            
+            <button class="opcao" name="cadastrar_novo_adm">Cadastrar novo ADM</button>
+
+            <button class="opcao" name="sair">Sair</button>
+                
+        </form>
+
+    </div>
+
+<?php
+
+if (isset($_POST['sair'])) {
+
+    header('Location: pagina_login.php');
+    
+}
+
+    extract($_POST);
+
+
+if (isset($ir_para_suap)) {
+
+    $url = "https://suap.ifba.edu.br/accounts/login/?next=/";
+    echo "<script> window.open('$url'); </script>";
+    exit;
+
+}
+
+
+if (isset($cadastrar_novo_adm)) {
+
+    header('Location: cadastro_adm.php');
+        
+
+}
+
+if (isset($status_requerimento)) {
+
+        
+
+}
+
+?>
+
+</body>
+
+</center>
+
+</html>
